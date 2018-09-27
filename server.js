584250f14c36c7cf3e62e7e03d012eb6216c5b15
Hoonji Baek <hoonji@korbit.co.kr>
2018-09-27 16:50:38 +0900

    add users model

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
-
-// DB Config
-const db = require('./config/keys').mongoURI;
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // Connect to MongoDB
+const db = require('./config/keys').mongoURI;
 mongoose
-  .connect(db)
+  .connect(db, {useNewUrlParser: true})
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
@@ -23,6 +24,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
